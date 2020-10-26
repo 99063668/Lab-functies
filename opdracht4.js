5cfc4703f07b7883a7662bf850c3bb66f95ab82f
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2020-10-26 11:11:24 +0100

    Add files via upload

--- opdracht4.js
+++ opdracht4.js
@@ -1,13 +1,292 @@
-const GETAL = 1;
+antwoord=prompt("Hoeveel tafels wilt u zien? 1 t/m 10")
 
-// De tafel van GETAL
-document.write('<br>1 x ' + GETAL + ' = ' + 1 * GETAL);
-document.write('<br>2 x ' + GETAL + ' = ' + 2 * GETAL);
-document.write('<br>3 x ' + GETAL + ' = ' + 3 * GETAL);
-document.write('<br>4 x ' + GETAL + ' = ' + 4 * GETAL);
-document.write('<br>5 x ' + GETAL + ' = ' + 5 * GETAL);
-document.write('<br>6 x ' + GETAL + ' = ' + 6 * GETAL);
-document.write('<br>7 x ' + GETAL + ' = ' + 7 * GETAL);
-document.write('<br>8 x ' + GETAL + ' = ' + 8 * GETAL);
-document.write('<br>9 x ' + GETAL + ' = ' + 9 * GETAL);
-document.write('<br>10 x' + GETAL + ' = ' + 10 * GETAL);
+if(antwoord=="1"){
+    // De tafel van 1
+    const GETAL = 1;
+    document.write('<br>1 x ' + GETAL + ' = ' + 1 * GETAL);
+    document.write('<br>2 x ' + GETAL + ' = ' + 2 * GETAL);
+    document.write('<br>3 x ' + GETAL + ' = ' + 3 * GETAL);
+    document.write('<br>4 x ' + GETAL + ' = ' + 4 * GETAL);
+    document.write('<br>5 x ' + GETAL + ' = ' + 5 * GETAL);
+    document.write('<br>6 x ' + GETAL + ' = ' + 6 * GETAL);
+    document.write('<br>7 x ' + GETAL + ' = ' + 7 * GETAL);
+    document.write('<br>8 x ' + GETAL + ' = ' + 8 * GETAL);
+    document.write('<br>9 x ' + GETAL + ' = ' + 9 * GETAL);
+    document.write('<br>10 x' + GETAL + ' = ' + 10 * GETAL);
+    }
+
+
+if(antwoord=="2"){
+    // De tafel van 1
+    const GETAL = 1;
+   document.write('<br>1 x ' + GETAL + ' = ' + 1 * GETAL);
+   document.write('<br>2 x ' + GETAL + ' = ' + 2 * GETAL);
+   document.write('<br>3 x ' + GETAL + ' = ' + 3 * GETAL);
+   document.write('<br>4 x ' + GETAL + ' = ' + 4 * GETAL);
+   document.write('<br>5 x ' + GETAL + ' = ' + 5 * GETAL);
+   document.write('<br>6 x ' + GETAL + ' = ' + 6 * GETAL);
+   document.write('<br>7 x ' + GETAL + ' = ' + 7 * GETAL);
+   document.write('<br>8 x ' + GETAL + ' = ' + 8 * GETAL);
+   document.write('<br>9 x ' + GETAL + ' = ' + 9 * GETAL);
+   document.write('<br>10 x' + GETAL + ' = ' + 10 * GETAL);
+   // De tafel van 2
+   document.write('<br>')
+   const getal = 2;
+   document.write('<br>1 x ' + getal + ' = ' + 1 * getal);
+   document.write('<br>2 x ' + getal + ' = ' + 2 * getal);
+   document.write('<br>3 x ' + getal + ' = ' + 3 * getal);
+   document.write('<br>4 x ' + getal + ' = ' + 4 * getal);
+   document.write('<br>5 x ' + getal + ' = ' + 5 * getal);
+   document.write('<br>6 x ' + getal + ' = ' + 6 * getal);
+   document.write('<br>7 x ' + getal + ' = ' + 7 * getal);
+   document.write('<br>8 x ' + getal + ' = ' + 8 * getal);
+   document.write('<br>9 x ' + getal + ' = ' + 9 * getal);
+   document.write('<br>10 x' + getal + ' = ' + 10 * getal);
+   }
+
+
+if(antwoord=="3"){
+    // De tafel van 1
+    const GETAL = 1;
+    document.write('<br>1 x ' + GETAL + ' = ' + 1 * GETAL);
+    document.write('<br>2 x ' + GETAL + ' = ' + 2 * GETAL);
+    document.write('<br>3 x ' + GETAL + ' = ' + 3 * GETAL);
+    document.write('<br>4 x ' + GETAL + ' = ' + 4 * GETAL);
+    document.write('<br>5 x ' + GETAL + ' = ' + 5 * GETAL);
+    document.write('<br>6 x ' + GETAL + ' = ' + 6 * GETAL);
+    document.write('<br>7 x ' + GETAL + ' = ' + 7 * GETAL);
+    document.write('<br>8 x ' + GETAL + ' = ' + 8 * GETAL);
+    document.write('<br>9 x ' + GETAL + ' = ' + 9 * GETAL);
+    document.write('<br>10 x' + GETAL + ' = ' + 10 * GETAL);
+    // De tafel van 2
+    document.write('<br>')
+    const getal = 2;
+    document.write('<br>1 x ' + getal + ' = ' + 1 * getal);
+    document.write('<br>2 x ' + getal + ' = ' + 2 * getal);
+    document.write('<br>3 x ' + getal + ' = ' + 3 * getal);
+    document.write('<br>4 x ' + getal + ' = ' + 4 * getal);
+    document.write('<br>5 x ' + getal + ' = ' + 5 * getal);
+    document.write('<br>6 x ' + getal + ' = ' + 6 * getal);
+    document.write('<br>7 x ' + getal + ' = ' + 7 * getal);
+    document.write('<br>8 x ' + getal + ' = ' + 8 * getal);
+    document.write('<br>9 x ' + getal + ' = ' + 9 * getal);
+    document.write('<br>10 x' + getal + ' = ' + 10 * getal);
+    // De tafel van 3
+    document.write('<br>')
+    const Getal = 3;
+    document.write('<br>1 x ' + Getal + ' = ' + 1 * Getal);
+    document.write('<br>2 x ' + Getal + ' = ' + 2 * Getal);
+    document.write('<br>3 x ' + Getal + ' = ' + 3 * Getal);
+    document.write('<br>4 x ' + Getal + ' = ' + 4 * Getal);
+    document.write('<br>5 x ' + Getal + ' = ' + 5 * Getal);
+    document.write('<br>6 x ' + Getal + ' = ' + 6 * Getal);
+    document.write('<br>7 x ' + Getal + ' = ' + 7 * Getal);
+    document.write('<br>8 x ' + Getal + ' = ' + 8 * Getal);
+    document.write('<br>9 x ' + Getal + ' = ' + 9 * Getal);
+    document.write('<br>10 x' + Getal + ' = ' + 10 * Getal);
+    }
+
+
+if(antwoord=="4"){
+    // De tafel van 1
+    const GETAL = 1;
+    document.write('<br>1 x ' + GETAL + ' = ' + 1 * GETAL);
+    document.write('<br>2 x ' + GETAL + ' = ' + 2 * GETAL);
+    document.write('<br>3 x ' + GETAL + ' = ' + 3 * GETAL);
+    document.write('<br>4 x ' + GETAL + ' = ' + 4 * GETAL);
+    document.write('<br>5 x ' + GETAL + ' = ' + 5 * GETAL);
+    document.write('<br>6 x ' + GETAL + ' = ' + 6 * GETAL);
+    document.write('<br>7 x ' + GETAL + ' = ' + 7 * GETAL);
+    document.write('<br>8 x ' + GETAL + ' = ' + 8 * GETAL);
+    document.write('<br>9 x ' + GETAL + ' = ' + 9 * GETAL);
+    document.write('<br>10 x' + GETAL + ' = ' + 10 * GETAL);
+    // De tafel van 2
+    document.write('<br>')
+    const getal = 2;
+    document.write('<br>1 x ' + getal + ' = ' + 1 * getal);
+    document.write('<br>2 x ' + getal + ' = ' + 2 * getal);
+    document.write('<br>3 x ' + getal + ' = ' + 3 * getal);
+    document.write('<br>4 x ' + getal + ' = ' + 4 * getal);
+    document.write('<br>5 x ' + getal + ' = ' + 5 * getal);
+    document.write('<br>6 x ' + getal + ' = ' + 6 * getal);
+    document.write('<br>7 x ' + getal + ' = ' + 7 * getal);
+    document.write('<br>8 x ' + getal + ' = ' + 8 * getal);
+    document.write('<br>9 x ' + getal + ' = ' + 9 * getal);
+    document.write('<br>10 x' + getal + ' = ' + 10 * getal);
+    // De tafel van 3
+    document.write('<br>')
+    const Getal = 3;
+    document.write('<br>1 x ' + Getal + ' = ' + 1 * Getal);
+    document.write('<br>2 x ' + Getal + ' = ' + 2 * Getal);
+    document.write('<br>3 x ' + Getal + ' = ' + 3 * Getal);
+    document.write('<br>4 x ' + Getal + ' = ' + 4 * Getal);
+    document.write('<br>5 x ' + Getal + ' = ' + 5 * Getal);
+    document.write('<br>6 x ' + Getal + ' = ' + 6 * Getal);
+    document.write('<br>7 x ' + Getal + ' = ' + 7 * Getal);
+    document.write('<br>8 x ' + Getal + ' = ' + 8 * Getal);
+    document.write('<br>9 x ' + Getal + ' = ' + 9 * Getal);
+    document.write('<br>10 x' + Getal + ' = ' + 10 * Getal);
+    // De tafel van 4
+    document.write('<br>')
+    const GEtal = 4;
+    document.write('<br>1 x ' + GEtal + ' = ' + 1 * GEtal);
+    document.write('<br>2 x ' + GEtal + ' = ' + 2 * GEtal);
+    document.write('<br>3 x ' + GEtal + ' = ' + 3 * GEtal);
+    document.write('<br>4 x ' + GEtal + ' = ' + 4 * GEtal);
+    document.write('<br>5 x ' + GEtal + ' = ' + 5 * GEtal);
+    document.write('<br>6 x ' + GEtal + ' = ' + 6 * GEtal);
+    document.write('<br>7 x ' + GEtal + ' = ' + 7 * GEtal);
+    document.write('<br>8 x ' + GEtal + ' = ' + 8 * GEtal);
+    document.write('<br>9 x ' + GEtal + ' = ' + 9 * GEtal);
+    document.write('<br>10 x' + GEtal + ' = ' + 10 * GEtal);
+    }
+
+
+    if(antwoord=="5"){
+    // De tafel van 1
+    const GETAL = 1;
+    document.write('<br>1 x ' + GETAL + ' = ' + 1 * GETAL);
+    document.write('<br>2 x ' + GETAL + ' = ' + 2 * GETAL);
+    document.write('<br>3 x ' + GETAL + ' = ' + 3 * GETAL);
+    document.write('<br>4 x ' + GETAL + ' = ' + 4 * GETAL);
+    document.write('<br>5 x ' + GETAL + ' = ' + 5 * GETAL);
+    document.write('<br>6 x ' + GETAL + ' = ' + 6 * GETAL);
+    document.write('<br>7 x ' + GETAL + ' = ' + 7 * GETAL);
+    document.write('<br>8 x ' + GETAL + ' = ' + 8 * GETAL);
+    document.write('<br>9 x ' + GETAL + ' = ' + 9 * GETAL);
+    document.write('<br>10 x' + GETAL + ' = ' + 10 * GETAL);
+    // De tafel van 2
+    document.write('<br>')
+    const getal = 2;
+    document.write('<br>1 x ' + getal + ' = ' + 1 * getal);
+    document.write('<br>2 x ' + getal + ' = ' + 2 * getal);
+    document.write('<br>3 x ' + getal + ' = ' + 3 * getal);
+    document.write('<br>4 x ' + getal + ' = ' + 4 * getal);
+    document.write('<br>5 x ' + getal + ' = ' + 5 * getal);
+    document.write('<br>6 x ' + getal + ' = ' + 6 * getal);
+    document.write('<br>7 x ' + getal + ' = ' + 7 * getal);
+    document.write('<br>8 x ' + getal + ' = ' + 8 * getal);
+    document.write('<br>9 x ' + getal + ' = ' + 9 * getal);
+    document.write('<br>10 x' + getal + ' = ' + 10 * getal);
+    // De tafel van 3
+    document.write('<br>')
+    const Getal = 3;
+    document.write('<br>1 x ' + Getal + ' = ' + 1 * Getal);
+    document.write('<br>2 x ' + Getal + ' = ' + 2 * Getal);
+    document.write('<br>3 x ' + Getal + ' = ' + 3 * Getal);
+    document.write('<br>4 x ' + Getal + ' = ' + 4 * Getal);
+    document.write('<br>5 x ' + Getal + ' = ' + 5 * Getal);
+    document.write('<br>6 x ' + Getal + ' = ' + 6 * Getal);
+    document.write('<br>7 x ' + Getal + ' = ' + 7 * Getal);
+    document.write('<br>8 x ' + Getal + ' = ' + 8 * Getal);
+    document.write('<br>9 x ' + Getal + ' = ' + 9 * Getal);
+    document.write('<br>10 x' + Getal + ' = ' + 10 * Getal);
+    // De tafel van 4
+    document.write('<br>')
+    const GEtal = 4;
+    document.write('<br>1 x ' + GEtal + ' = ' + 1 * GEtal);
+    document.write('<br>2 x ' + GEtal + ' = ' + 2 * GEtal);
+    document.write('<br>3 x ' + GEtal + ' = ' + 3 * GEtal);
+    document.write('<br>4 x ' + GEtal + ' = ' + 4 * GEtal);
+    document.write('<br>5 x ' + GEtal + ' = ' + 5 * GEtal);
+    document.write('<br>6 x ' + GEtal + ' = ' + 6 * GEtal);
+    document.write('<br>7 x ' + GEtal + ' = ' + 7 * GEtal);
+    document.write('<br>8 x ' + GEtal + ' = ' + 8 * GEtal);
+    document.write('<br>9 x ' + GEtal + ' = ' + 9 * GEtal);
+    document.write('<br>10 x' + GEtal + ' = ' + 10 * GEtal);
+    // De tafel van 5
+    document.write('<br>')
+    const GETal = 5;
+    document.write('<br>1 x ' + GETal + ' = ' + 1 * GETal);
+    document.write('<br>2 x ' + GETal + ' = ' + 2 * GETal);
+    document.write('<br>3 x ' + GETal + ' = ' + 3 * GETal);
+    document.write('<br>4 x ' + GETal + ' = ' + 4 * GETal);
+    document.write('<br>5 x ' + GETal + ' = ' + 5 * GETal);
+    document.write('<br>6 x ' + GETal + ' = ' + 6 * GETal);
+    document.write('<br>7 x ' + GETal + ' = ' + 7 * GETal);
+    document.write('<br>8 x ' + GETal + ' = ' + 8 * GETal);
+    document.write('<br>9 x ' + GETal + ' = ' + 9 * GETal);
+    document.write('<br>10 x' + GETal + ' = ' + 10 * GETal);
+    }
+
+
+    if(antwoord=="6"){
+    // De tafel van 1
+    const GETAL = 1;
+    document.write('<br>1 x ' + GETAL + ' = ' + 1 * GETAL);
+    document.write('<br>2 x ' + GETAL + ' = ' + 2 * GETAL);
+    document.write('<br>3 x ' + GETAL + ' = ' + 3 * GETAL);
+    document.write('<br>4 x ' + GETAL + ' = ' + 4 * GETAL);
+    document.write('<br>5 x ' + GETAL + ' = ' + 5 * GETAL);
+    document.write('<br>6 x ' + GETAL + ' = ' + 6 * GETAL);
+    document.write('<br>7 x ' + GETAL + ' = ' + 7 * GETAL);
+    document.write('<br>8 x ' + GETAL + ' = ' + 8 * GETAL);
+    document.write('<br>9 x ' + GETAL + ' = ' + 9 * GETAL);
+    document.write('<br>10 x' + GETAL + ' = ' + 10 * GETAL);
+    // De tafel van 2
+    document.write('<br>')
+    const getal = 2;
+    document.write('<br>1 x ' + getal + ' = ' + 1 * getal);
+    document.write('<br>2 x ' + getal + ' = ' + 2 * getal);
+    document.write('<br>3 x ' + getal + ' = ' + 3 * getal);
+    document.write('<br>4 x ' + getal + ' = ' + 4 * getal);
+    document.write('<br>5 x ' + getal + ' = ' + 5 * getal);
+    document.write('<br>6 x ' + getal + ' = ' + 6 * getal);
+    document.write('<br>7 x ' + getal + ' = ' + 7 * getal);
+    document.write('<br>8 x ' + getal + ' = ' + 8 * getal);
+    document.write('<br>9 x ' + getal + ' = ' + 9 * getal);
+    document.write('<br>10 x' + getal + ' = ' + 10 * getal);
+    // De tafel van 3
+    document.write('<br>')
+    const Getal = 3;
+    document.write('<br>1 x ' + Getal + ' = ' + 1 * Getal);
+    document.write('<br>2 x ' + Getal + ' = ' + 2 * Getal);
+    document.write('<br>3 x ' + Getal + ' = ' + 3 * Getal);
+    document.write('<br>4 x ' + Getal + ' = ' + 4 * Getal);
+    document.write('<br>5 x ' + Getal + ' = ' + 5 * Getal);
+    document.write('<br>6 x ' + Getal + ' = ' + 6 * Getal);
+    document.write('<br>7 x ' + Getal + ' = ' + 7 * Getal);
+    document.write('<br>8 x ' + Getal + ' = ' + 8 * Getal);
+    document.write('<br>9 x ' + Getal + ' = ' + 9 * Getal);
+    document.write('<br>10 x' + Getal + ' = ' + 10 * Getal);
+    // De tafel van 4
+    document.write('<br>')
+    const GEtal = 4;
+    document.write('<br>1 x ' + GEtal + ' = ' + 1 * GEtal);
+    document.write('<br>2 x ' + GEtal + ' = ' + 2 * GEtal);
+    document.write('<br>3 x ' + GEtal + ' = ' + 3 * GEtal);
+    document.write('<br>4 x ' + GEtal + ' = ' + 4 * GEtal);
+    document.write('<br>5 x ' + GEtal + ' = ' + 5 * GEtal);
+    document.write('<br>6 x ' + GEtal + ' = ' + 6 * GEtal);
+    document.write('<br>7 x ' + GEtal + ' = ' + 7 * GEtal);
+    document.write('<br>8 x ' + GEtal + ' = ' + 8 * GEtal);
+    document.write('<br>9 x ' + GEtal + ' = ' + 9 * GEtal);
+    document.write('<br>10 x' + GEtal + ' = ' + 10 * GEtal);
+    // De tafel van 5
+    document.write('<br>')
+    const GETal = 5;
+    document.write('<br>1 x ' + GETal + ' = ' + 1 * GETal);
+    document.write('<br>2 x ' + GETal + ' = ' + 2 * GETal);
+    document.write('<br>3 x ' + GETal + ' = ' + 3 * GETal);
+    document.write('<br>4 x ' + GETal + ' = ' + 4 * GETal);
+    document.write('<br>5 x ' + GETal + ' = ' + 5 * GETal);
+    document.write('<br>6 x ' + GETal + ' = ' + 6 * GETal);
+    document.write('<br>7 x ' + GETal + ' = ' + 7 * GETal);
+    document.write('<br>8 x ' + GETal + ' = ' + 8 * GETal);
+    document.write('<br>9 x ' + GETal + ' = ' + 9 * GETal);
+    document.write('<br>10 x' + GETal + ' = ' + 10 * GETal);
+    // De tafel van 6
+    document.write('<br>')
+    const GETAl = 6;
+    document.write('<br>1 x ' + GETAl + ' = ' + 1 * GETAl);
+    document.write('<br>2 x ' + GETAl + ' = ' + 2 * GETAl);
+    document.write('<br>3 x ' + GETAl + ' = ' + 3 * GETAl);
+    document.write('<br>4 x ' + GETAl + ' = ' + 4 * GETAl);
+    document.write('<br>5 x ' + GETAl + ' = ' + 5 * GETAl);
+    document.write('<br>6 x ' + GETAl + ' = ' + 6 * GETAl);
+    document.write('<br>7 x ' + GETAl + ' = ' + 7 * GETAl);
+    document.write('<br>8 x ' + GETAl + ' = ' + 8 * GETAl);
+    document.write('<br>9 x ' + GETAl + ' = ' + 9 * GETAl);
+    document.write('<br>10 x' + GETAl + ' = ' + 10 * GETAl);
+    }
+    
