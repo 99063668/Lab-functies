e4829c4bb43d0ed3590c8d9fa4ecee604eb06bca
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2020-10-26 13:56:27 +0100

    Add files via upload

--- opdracht3.js
+++ opdracht3.js
@@ -1,22 +1,14 @@
 //Naam
-/*antwoord=prompt("Wat is je naam?");
-
 function hallo(naam){ 
-    document.write('Hallo'+naam+'!<br>');
+    document.write('<br>Hallo' + prompt('Wat is je naam?'));
  }
 var NaamPersoon='Jan';
  hallo(NaamPersoon);
 
 
 //Leeftijd
-antwoord=prompt("Wat is je leeftijd?");
-
 function Leeftijd(leeftijd){ 
-    document.write('Je leeftijd is'+leeftijd+'jaar<br>');
+    document.write('<br>Je leeftijd is' + prompt('Wat is je leeftijd?'));
  }
 var LeeftijdPersoon='16';
- Leeftijd(LeeftijdPersoon);*/
-
-
- document.write('<br>Hallo' + prompt('Wat is je naam?'));
- document.write('<br>Je leeftijd is' + prompt('Wat is je leeftijd?'));
+ Leeftijd(LeeftijdPersoon);
